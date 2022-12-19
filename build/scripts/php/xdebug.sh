65be96f1ea05f7161097e988e0871b5e7c532803
Can Gelis <geliscan@gmail.com>
2022-12-19 11:58:19 +0300

    ide key to xdebug

--- build/scripts/php/xdebug.sh
+++ build/scripts/php/xdebug.sh
@@ -6,6 +6,8 @@ alpine/dev.sh
 
 pecl install xdebug
 
-echo "xdebug.remote_host=host.docker.internal" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
+echo "xdebug.remote_connect_back=1" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
 echo "xdebug.remote_enable=on" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
 echo "xdebug.auto_start=off" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
+echo "xdebug.default_enable=1" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
+echo "xdebug.idekey=PHPSTORM" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
