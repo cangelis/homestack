354ee3243ae45da5e99566d51493a8080ccbfeb9
Can Gelis <geliscan@gmail.com>
2022-12-19 11:57:35 +0300

    Fix 7.4 incompatibility on GD

--- build/scripts/php/gd.sh
+++ build/scripts/php/gd.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
-apk add libpng-dev libwebp-dev libjpeg-turbo-dev freetype-dev
-docker-php-ext-configure gd --with-freetype-dir=/usr/include/ \
+apk add libpng-dev libwebp-dev libjpeg-turbo-dev freetype-dev zlib-dev
+
+IS_NEWER_THAN_74=$(php -r "echo version_compare(PHP_VERSION, '7.4.0') >= 0 ? 'true' : 'false';")
+
+if [ $IS_NEWER_THAN_74 = "true" ]; then
+    docker-php-ext-configure gd --with-jpeg --with-freetype --with-webp
+else
+    docker-php-ext-configure gd --with-freetype-dir=/usr/include/ \
     --with-webp-dir=/usr/include/ \
     --with-jpeg-dir=/usr/include/ \
     --with-png-dir=/usr/include
+fi
+
 docker-php-ext-install gd
